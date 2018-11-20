c898ae366b39f1b1a4509e23a129999bdd95efcf
Mark Locklear <marklocklear@gmail.com>
2018-11-20 14:25:03 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { SecureComponent } from "./components/secure/secure.component";
+import { AppService } from "./app.service";
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        NativeScriptFormsModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+        SecureComponent
+    ],
+    providers: [
+        AppService
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class AppModule { }
